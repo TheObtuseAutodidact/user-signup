92bb5c1552d30d010a76a84386a8f5625917b6d5
Obtuse <theobtuseautodidact@gmail.com>
2017-07-08 07:06:05 -0700

    Refactor a bit of basic functionality on long-hand implementation

--- main.py
+++ main.py
@@ -14,35 +14,41 @@ def is_valid(field):
 def is_valid_email(email):
     if not is_valid(email):
         return False
-    if "." not in email and "@" not in email and " " in email:
+    if "." not in email or "@" not in email or " " in email:
         return False
     return True
+
+def validate(inputs, errors):
+    if not is_valid(inputs['username']):
+            errors['username']="That's not a valid username"
+    if not is_valid(inputs['password']):
+        errors["password"] = "That's not a valid password"
+    if inputs['password'] != inputs['verify_pass'] or not inputs['verify_pass']:
+        errors["match_password"] = "Passwords don't match"
+    if inputs['email']:
+        if not is_valid_email(inputs['email']):
+            errors["email"] = "That's not a valid email"
+
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("index_form.html")
-    if request.method == "POST":
 
+    if request.method == "POST":
         username = request.form['user-name'].strip()
         password = request.form['password'].strip()
         verify_pass = request.form['verify-pass'].strip()
         email = request.form['email'].strip()
+        field_inputs = {'username':username, 'password':password, 'verify_pass':verify_pass, 'email':email}
         errors = {}
-        if not is_valid(username):
-            errors['username']="That's not a valid username"
-        if not is_valid(password):
-            errors["password"] = "That's not a valid password"
-        if password != verify_pass or not verify_pass:
-            errors["match_password"] = "Passwords don't match"
-        if email:
-            if not is_valid_email(email):
-                errors["email"] = "That's not a valid email"
+
+        validate(field_inputs, errors)
         
         if errors:
-            form_field = {'username': username, "email": email}
-            return render_template("index_form.html", errors=errors, form_field=form_field)
+            form_fields = {'username': username, "email": email}
+            return render_template("index_form.html", errors=errors, form_fields=form_fields)
         return "Welcome, " + request.form["user-name"]
 
 app.run()
